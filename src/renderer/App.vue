0ebfb027a18ca995efdf30a187ffbc2c36175c7e
wangshaohua <376220459@qq.com>
2019-07-19 14:58:24 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'together'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
